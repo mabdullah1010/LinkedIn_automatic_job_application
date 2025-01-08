a1fe0a0e13963d73b59d5213a9e9fb2a75df13f6
Muhammad Abdullah <mabdullah@conncoll.edu>
2025-01-07 21:15:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 print("Automatic Job Application via LinkedIn")
